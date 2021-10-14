8a6e88f91b223d4711fb97258c0118a75a77771d
OceanLeonAI <beyondjayfir@126.com>
2021-10-14 09:56:31 +0800

    Java 加密解密 AESWithCBC 添加

---
+++ hello-encrypt-decrypt/src/main/java/com/leon/asymmetric/aes/AESWithCBC.java
@@ -0,0 +1,64 @@
+package com.leon.asymmetric.aes;
+
+import org.apache.commons.codec.binary.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+/**
+ * @PROJECT_NAME: hello-tools
+ * @CLASS_NAME: AESWithCBC
+ * @AUTHOR: OceanLeonAI
+ * @CREATED_DATE: 2021/10/14 9:53
+ * @Version 1.0
+ * @DESCRIPTION:
+ **/
+public class AESWithCBC {
+
+    private static final String key = "aesEncryptionKey";
+    private static final String initVector = "encryptionIntVec";
+
+    public static String encrypt(String value) {
+        try {
+            IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
+            SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+            cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
+
+            byte[] encrypted = cipher.doFinal(value.getBytes());
+            return Base64.encodeBase64String(encrypted);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String decrypt(String encrypted) {
+        try {
+            IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
+            SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+            cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
+            byte[] original = cipher.doFinal(Base64.decodeBase64(encrypted));
+
+            return new String(original);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+//        String originalString = "password";
+        String originalString = "这个是一段待加密的文字，随便写写，没有特别要求";
+        System.out.println("Original String to encrypt - " + originalString);
+        String encryptedString = encrypt(originalString);
+        System.out.println("Encrypted String - " + encryptedString);
+        String decryptedString = decrypt(encryptedString);
+        System.out.println("After decryption - " + decryptedString);
+    }
+}
